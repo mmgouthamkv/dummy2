d421b78ce6c5d9b0f945bae9776c0a9df3fee3e3
mmgouthamkv <79371691+mmgouthamkv@users.noreply.github.com>
2024-10-04 15:58:53 +0530

    Create ServiceBController.java

---
+++ ServiceBController.java
@@ -0,0 +1,18 @@
+package com.example.serviceb;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+@RestController
+public class ServiceBController {
+   @Autowired
+   private RestTemplate restTemplate;
+   @GetMapping("/service-b")
+   public String callServiceA() {
+       // Call to ServiceA
+       String url = "http://localhost:8080/service-a";
+       ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+       return "Response from ServiceA: " + response.getBody();
+   }
+}
